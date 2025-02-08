7e2c925e1eeb0e56d96036f302a5652cdc345605
Luiz Vinicius Lima da Silva <luizviniciuslimadasilva@gmail.com>
2025-02-07 22:07:26 -0300

    Add files via upload

---
+++ OperadoreRelacionais-Logicos/Operadores-Funcoes/EstruturasCondicionais-ifelse/script.js
@@ -0,0 +1,48 @@
+/* Estruturas condicionais if else
+  - Verificar idade para entrar em site
+  - Verificar se a compra é maior do
+  que o mínimo
+  - Verificar se tem acesso a recursos 
+  pagos
+  */
+
+/*
+ let idade = 17
+ let compras = 400
+ 
+ if( idade >= 18 && idade <= 25) {
+    console.log("Maior de idade")
+
+ }else{
+    console.log("Menor de idade")
+
+ }
+*/
+
+/*
+let idade = 20;
+let compras = 200;
+
+if (idade >= 40 || compras >= 400) {
+  console.log("Desconto de 20%");
+
+}else{
+    console.log("Não tem desconto");
+}
+    */
+
+let opcao = 1 
+
+if( opcao == 1 ) {
+    console.log("Saldo conta");
+
+}else if ( opcao == 2) {
+    console.log("Cartão de crédito");
+
+}else if ( opcao == 3) {
+    console.log("Descontos indevidos");
+
+}else{
+    console.log("Opção inválida");
+
+}
