089fea949ae4ba247dd5cee02bb3bda068615cad
Luiz Vinicius Lima da Silva <luizviniciuslimadasilva@gmail.com>
2025-02-09 19:53:56 -0300

    Update script.js

--- OperadoreRelacionais-Logicos/Funções/script.js
+++ OperadoreRelacionais-Logicos/Funções/script.js
@@ -2,6 +2,7 @@
 1) Desligar a torneira;
 2) Pedir copo de água;
 3) Ir ao mercado comprar arroz;
+4) Calcular média;
 */
 
 //1) Desligar a torneira;
@@ -24,6 +25,20 @@ function irMercadoComprarArroz(dinheiro) {
     return "arroz"
     
 }
+
+//4) Calcular média;
+function calcularMedia(nota1, nota2) {
+
+    let totalNotas = nota1 + nota2
+    let media = totalNotas / 2
+
+    return media
+    
+}
+
+let media = calcularMedia(8,8)
+console.log(media)
+
 
 let retorno = irMercadoComprarArroz(10)
 console.log(retorno)
