9ae7949b5afaa59f3e16c4e92dc00870595ac89f
Luiz Vinicius Lima da Silva <luizviniciuslimadasilva@gmail.com>
2025-04-20 08:16:15 -0300

    Add files via upload

---
+++ JavaScript-Arrays-Loops/Arrays/Arrays.js
@@ -0,0 +1,13 @@
+let nome = "vinih";
+let nomes = ["Vinih", "Jamilton", "Maria"];
+
+// ADICIONAR NOVO INTEM
+//nomes.push("Novo item");
+
+// ALTERAR ITEM EXISTENTE
+//nomes[0] = "teste";
+
+// DELETAR ITEM EXISTENTE
+delete nome[0];
+
+console.log(nomes[0]);
