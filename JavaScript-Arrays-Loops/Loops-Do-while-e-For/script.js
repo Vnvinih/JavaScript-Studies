12d6879e44f11d598f00c36f3d4d24245875827b
Luiz Vinicius Lima da Silva <luizviniciuslimadasilva@gmail.com>
2025-04-25 17:35:24 -0300

    Add files via upload

---
+++ JavaScript-Arrays-Loops/Loops-Do-while-e-For/script.js
@@ -0,0 +1,37 @@
+//while
+
+/*let numero = 1;
+while (numero <= 5) {
+  console.log("Executou " + numero);
+  //numero = numero + 1;
+  numero++
+
+}*/
+
+//Do...while
+
+/*let numero = 1;
+do {
+  console.log("Executou " + numero);
+  numero++;
+} while (numero <= 5);
+*/
+
+//For
+let postagens = [
+  "Hoje passeando por Recife",
+  "Passeando pelo antigo",
+  "Hoje fiz um curso de back-end",
+  "Estudando Java!",
+];
+
+for (indice in postagens) {
+  console.log(postagens[indice]);
+}
+
+/*
+let total = postagens.length;
+for (let numero = 0; numero < total; numero++) {
+  console.log(postagens[numero]);
+}
+*/
