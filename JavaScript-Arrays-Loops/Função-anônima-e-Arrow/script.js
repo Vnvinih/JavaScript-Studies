3e6da8a75eaf4c95f18f24cf9734191631f0b737
Luiz Vinicius Lima da Silva <luizviniciuslimadasilva@gmail.com>
2025-04-28 18:35:27 -0300

    Update script.js

--- JavaScript-Arrays-Loops/Função-anônima-e-Arrow/script.js
+++ JavaScript-Arrays-Loops/Função-anônima-e-Arrow/script.js
@@ -1,7 +1,27 @@
-// funçao
+/* funçao
+function somar(a, b) {
+  console.log(a + b);
+}
+
+somar(1 + 2);*/
 
 // + Função anônima (sem nome)
+/*const somar = function (a, b) {
+  console.log(a + b);
+};
+somar(1, 2);*/
 
 // + Função arrow (seta)
+/*const somar = (a, b) => {
+  console.log(a + b);
+};
+somar(1, 2);
+*/
 
 // + Função arrow (retorno immplicito)
+//const somar = (a, b) => console.log(a + b);
+//const exibir = (n) => console.log("nome: " + n);
+const somar = (a, b) => a + b;
+//exibir("teste")
+let resultado = somar(1, 2);
+console.log(resultado);
