c134afcba13d5bf1cc0ed030fd1cea5225b7d80a
Luiz Vinicius Lima da Silva <luizviniciuslimadasilva@gmail.com>
2025-06-16 19:36:41 -0300

    Update script.js

--- JavaScript-Orientação-a-Objetos/Pilares-Da-Orientação-a-Objetos-Herança/script.js
+++ JavaScript-Orientação-a-Objetos/Pilares-Da-Orientação-a-Objetos-Herança/script.js
@@ -10,6 +10,9 @@ class Animal {
   }
   correr() {
     console.log("correr");
+    console.log("como");
+    console.log("um");
+    // 30 linhas
   }
 
   dormir() {
@@ -19,6 +22,16 @@ class Animal {
 
 class Cao extends Animal {
   // SubClasse
+  constructor() {
+    super();
+    this.tamanhoOrelha = 0;
+  }
+  correr() {
+    super.correr();
+    console.log("cao");
+    // 5 linhas
+  }
+
   latir() {
     console.log("latir");
   }
@@ -31,14 +44,29 @@ class Passaro extends Animal {
   }
 }
 
+class Papagaio extends Passaro {
+  falar() {
+    console.log("falar");
+  }
+}
+
 // Instancia
 const cao = new Cao();
-const passaro = new Passaro();
+// const passaro = new Passaro();
+// const papagaio = new Papagaio();
+
+cao.correr();
+
+/*
+papagaio.correr(); // Animal
+papagaio.voar();
+papagaio.falar();
+*/
 
 // cao.correr();
 // cao.latir();
-passaro.voar();
-passaro.correr();
+// passaro.voar();
+// passaro.correr();
 //passaro.cor = "Amarelo";
 //console.log(passaro.cor);
 
